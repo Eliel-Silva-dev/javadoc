9a89ca842eacfe4c5f17456459a311e8db766b1d
Eliel-Silva-dev <eliellnr@hotmail.com>
2025-09-15 14:32:18 -0300

    doc 091525 hora
    projeto hora do sistema

---
+++ HoraDoSitema/src/main/java/com/mycompany/horadositema/HoraDoSitema.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.horadositema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Eliel
+ */
+public class HoraDoSitema {
+
+    public static void main(String[] args) {
+        Date data = new Date();
+        
+        System.out.println("A hora atual do sistema é: ");
+        System.out.println(data.toString());
+        
+    }
+}
