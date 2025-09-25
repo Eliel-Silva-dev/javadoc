92b71b94252ab2eb53fea9a2af2d482607f6de80
Eliel-Silva-dev <eliellnr@hotmail.com>
2025-09-25 18:53:56 -0300

    operadores 250925
    operadores aritimeticos

---
+++ OpetadoresAritimeticos/src/main/java/com/mycompany/opetadoresaritimeticos/OpetadoresAritimeticos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.opetadoresaritimeticos;
+
+/**
+ *
+ * @author Eliel
+ */
+public class OpetadoresAritimeticos {
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float media = (n1 + n2) / 2;
+        
+        System.out.println("A média é: " + media);
+    }
+}
