f21f8242fc13f37ee61813cf78aa0e4cbcb3bfee
Eliel-Silva-dev <eliellnr@hotmail.com>
2025-12-12 14:44:10 -0300

    calculadora java
    projeto de calculadora em java

---
+++ CalculadoraJava/src/calculadoraJava/CalculadoraJava.java
@@ -0,0 +1,11 @@
+package calculadoraJava;
+
+public class CalculadoraJava {
+	// projeto de calculadora em Java
+	
+	public static void main(String[] args) {
+		System.out.println("Abertura do projeto de calculadora em java");
+		
+	}
+
+}
