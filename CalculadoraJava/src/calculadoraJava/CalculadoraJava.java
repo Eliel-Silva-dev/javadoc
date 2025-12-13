80783292b8eff5a2a42f8587f536bb57c95cb403
Eliel-Silva-dev <eliellnr@hotmail.com>
2025-12-13 16:08:27 -0300

    switch
    switch

--- CalculadoraJava/src/calculadoraJava/CalculadoraJava.java
+++ CalculadoraJava/src/calculadoraJava/CalculadoraJava.java
@@ -4,7 +4,28 @@ public class CalculadoraJava {
 	// projeto de calculadora em Java
 	
 	public static void main(String[] args) {
-		System.out.println("Abertura do projeto de calculadora em java");
+		int pos = 7;
+		
+		switch(pos) {
+			case 1:
+				System.out.println("Primeira posição");
+				break;
+			case 2:
+				System.out.println("Segunda posição");
+				break;
+			case 3:
+				System.out.println("Terceira posição");
+				break;
+			case 4:
+				System.out.println("Quarta posição");
+				break;
+			case 5:
+				System.out.println("Quinta posição");
+				break;
+			default:
+				System.out.println("Você não obteve pontos suficientes");
+				break;
+		}
 		
 	}
 
