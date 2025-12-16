2ea7185545e332244c2aaa5221069d3d62f3bbf2
Eliel-Silva-dev <eliellnr@hotmail.com>
2025-12-16 12:57:49 -0300

    loops 161225
    loop for

---
+++ Loops/src/para/Para.java
@@ -0,0 +1,19 @@
+package para;
+
+public class Para {
+	
+	// Usado quando se sabe quantas vezes o loop será execultado. 
+	
+	public static void main(String[] args) {
+		
+		for (int cont = 0; cont < 5; cont++) {
+		
+			System.out.println((cont + 1) + " - Posição...");
+		}
+		
+
+		System.out.println("\nFim do loop");
+	}
+		
+
+}
