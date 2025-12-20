f7bfef0dd91d8ec0a048c5dd3bbebeb3e6befb8c
Eliel-Silva-dev <eliellnr@hotmail.com>
2025-12-20 15:17:57 -0300

    controle 201225
    estrutura de controle

---
+++ controle/src/escolhaCaso/EscolhaCaso.java
@@ -0,0 +1,29 @@
+package escolhaCaso;
+
+public class EscolhaCaso {
+
+	public static void main(String[] args) {
+		int pos = 7;
+		
+		switch(pos) {
+			case 1:
+				System.out.println("Primeira posição");
+				break;
+			case 2:
+				System.out.println("Segunda posição");
+				break;
+			case 3:
+				System.out.println("Terceira posição");
+				break;
+			case 4:
+				System.out.println("Quarta posição");
+				break;
+			case 5:
+				System.out.println("Quinta posição");
+				break;
+			default:
+				System.out.println("Você não obteve pontos suficientes");
+				break;
+		}
+	}
+}
