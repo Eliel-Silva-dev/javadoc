e8b29a202e1f32661522d62365ffd0b6ca13fe22
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-02 13:57:43 -0300

    doc: primero projeto spring
    Construindo o primeiro projeto em spring

---
+++ primeiro-projeto-spring/src/test/java/com/elielSilvaDev/primeiro_projeto_spring/PrimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elielSilvaDev.primeiro_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
