1e182d417910fb30df5fd355d4aa410b12ac3309
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-04 17:58:17 -0300

    feat: controller person
    get methods. request all data and search by Id.

--- primeiro-projeto-spring/src/main/java/com/elielSilvaDev/primeiro_projeto_spring/PersonServices.java
+++ primeiro-projeto-spring/src/main/java/com/elielSilvaDev/primeiro_projeto_spring/PersonServices.java
@@ -3,6 +3,8 @@ package com.elielSilvaDev.primeiro_projeto_spring;
 import com.elielSilvaDev.primeiro_projeto_spring.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,17 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+
+            persons.add(person);
+        }
+
+        return persons;
+    }
 
     public Person findById(String id) {
         logger.info("Finding on person");
@@ -24,4 +37,16 @@ public class PersonServices {
 
         return person;
     }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("firstName" + i);
+        person.setLastName("LastName" + i);
+        person.setAddress("Brasil");
+        person.setGender(i % 2 == 0 ? "MAle":"Female");
+
+        return person;
+    }
+
 }
