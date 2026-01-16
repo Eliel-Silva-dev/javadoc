5a698438d4e32e81d3479426079071da57406e4c
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-16 14:06:30 -0300

    feat: create model Film

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/Film.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/Film.java
@@ -1,4 +1,13 @@
 package br.com.alura.screenmatch.model;
 
-public class Film {
+public class Film extends Title{
+    private String director;
+
+    public String getDirector() {
+        return this.director;
+    }
+
+    public  void setDirector(String director) {
+        this.director = director;
+    }
 }
