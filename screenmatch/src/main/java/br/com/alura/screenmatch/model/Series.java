4017ecc07047be7d17f5d6cb8db6bbcadcd0a0c5
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-16 14:13:43 -0300

    feat: create model series

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/Series.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/Series.java
@@ -1,4 +1,45 @@
 package br.com.alura.screenmatch.model;
 
-public class Series {
+public class Series extends Title{
+    private int temporadas;
+    private boolean ativa;
+    private int episodeosPorTemporada;
+    private int minutosPorEpisodeo;
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodeosPorTemporada * minutosPorEpisodeo;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodeosPorTemporada() {
+        return episodeosPorTemporada;
+    }
+
+    public void setEpisodeosPorTemporada(int episodeosPorTemporada) {
+        this.episodeosPorTemporada = episodeosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodeo() {
+        return minutosPorEpisodeo;
+    }
+
+    public void setMinutosPorEpisodeo(int minutosPorEpisodeo) {
+        this.minutosPorEpisodeo = minutosPorEpisodeo;
+    }
 }
