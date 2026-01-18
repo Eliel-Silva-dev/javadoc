8ed936060da4216e614435c06631a5b0af7a6539
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-17 22:31:52 -0300

    feat: entity credit card

---
+++ comprasCard/src/main/java/CreditCard.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditCard {
+    private double limit;
+    private double balance;
+    private List<Purchase> purchases;
+
+    public CreditCard(double limit) {
+        this.limit = limit;
+        this.balance = limit;
+        this.purchases = new ArrayList<>();
+    }
+
+    public boolean lancaCampo(Purchase purchase) {
+
+    }
+    public double getLimit() {
+        return limit;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Purchase> getCompras() {
+        return compras;
+    }
+}
