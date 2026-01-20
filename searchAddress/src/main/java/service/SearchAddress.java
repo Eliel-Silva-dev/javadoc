c9f29355b7942b800906c46c1ac8a3866c910766
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-20 16:27:00 -0300

    feat: service searchAddress

--- searchAddress/src/main/java/service/SearchAddress.java
+++ searchAddress/src/main/java/service/SearchAddress.java
@@ -1,11 +1,28 @@
 package service;
 
+import com.google.gson.Gson;
 import model.Adress;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class SearchAddress {
 
-    public Adress searchCep (String cep) {
+    public Adress searchCep(String cep) {
 
-        return ;
+        URI uriLocation = URI.create("https://viacep.com.br/" + cep + "/json/");
+
+        HttpRequest request = HttpRequest.newBuilder().uri(uriLocation).build();
+
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            return new Gson().fromJson(response.body(), Adress.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Não consegui obter o endereço a partir desse CEP.");
+        }
     }
 }
