c9b95ba4b0cbd61b55d24086ccc9c2044015b277
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-20 15:52:54 -0300

    feat: model adress

---
+++ searchAdress/src/main/java/model/Adress.java
@@ -0,0 +1,9 @@
+package model;
+
+public record Adress(String cep,
+                     String logradouro,
+                     String complemento,
+                     String bairro,
+                     String localidade,
+                     String uf) {
+}
