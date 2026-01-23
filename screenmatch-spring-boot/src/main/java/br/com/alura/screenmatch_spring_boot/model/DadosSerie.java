168e753c6e86242d9cea576c29a18bfb4e5c90e4
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-22 21:16:55 -0300

    feat: model dados serie

---
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/model/DadosSerie.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch_spring_boot.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(
+        @JsonAlias String titulo,
+        @JsonAlias String totalTemporadas,
+        @JsonAlias String avaliacao
+) {
+}
