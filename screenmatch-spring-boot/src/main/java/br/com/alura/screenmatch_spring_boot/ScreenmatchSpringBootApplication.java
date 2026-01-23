565a69d8cad64ebd51ab8e486ecad606584912f1
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-22 21:12:40 -0300

    docs: add dependency jackson

--- screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/ScreenmatchSpringBootApplication.java
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/ScreenmatchSpringBootApplication.java
@@ -1,13 +1,18 @@
 package br.com.alura.screenmatch_spring_boot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchSpringBootApplication {
+public class ScreenmatchSpringBootApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchSpringBootApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("teste");
+	}
 }
