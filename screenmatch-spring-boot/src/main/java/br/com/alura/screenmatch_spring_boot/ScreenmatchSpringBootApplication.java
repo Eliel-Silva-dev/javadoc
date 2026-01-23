1c0193036732b190c195a4b519b266328f5344a9
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-22 21:05:33 -0300

    feat: new project screenmatch with spring boot

---
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/ScreenmatchSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchSpringBootApplication.class, args);
+	}
+
+}
---
+++ screenmatch-spring-boot/src/test/java/br/com/alura/screenmatch_spring_boot/ScreenmatchSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
