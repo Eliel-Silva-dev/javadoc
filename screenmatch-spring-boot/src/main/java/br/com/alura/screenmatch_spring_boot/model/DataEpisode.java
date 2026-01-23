3cb31815bec77b67963f035e6d05da6377eb8385
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-23 17:12:32 -0300

    feat: new record episode

---
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/model/DataEpisode.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch_spring_boot.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataEpisode(
+        @JsonAlias("title") String title,
+        @JsonAlias("Episode") Integer number,
+        @JsonAlias("imdbRating") String rating,
+        @JsonAlias("Realeased") String ReleaseDate
+) {
+}
