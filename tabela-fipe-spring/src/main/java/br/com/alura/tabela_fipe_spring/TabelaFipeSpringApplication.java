32117e472e429a1505710fc0dd2b940fff6e4705
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-24 19:47:54 -0300

    docs: arquivos iniciais do projeto

---
+++ tabela-fipe-spring/src/main/java/br/com/alura/tabela_fipe_spring/TabelaFipeSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.tabela_fipe_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabelaFipeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabelaFipeSpringApplication.class, args);
+	}
+
+}
---
+++ tabela-fipe-spring/src/test/java/br/com/alura/tabela_fipe_spring/TabelaFipeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.tabela_fipe_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
