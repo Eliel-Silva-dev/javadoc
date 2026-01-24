79f77ae8435c99b80d35882623a0a56488f31865
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-24 19:59:29 -0300

    refact: rename variable modelos

---
+++ tabela-fipe-spring/src/main/java/br/com/alura/tabela_fipe_spring/model/Data.java
@@ -0,0 +1,4 @@
+package br.com.alura.tabela_fipe_spring.model;
+
+public record Data(String code, String name) {
+}
--- tabela-fipe-spring/src/main/java/br/com/alura/tabela_fipe_spring/model/Modelos.java
+++ tabela-fipe-spring/src/main/java/br/com/alura/tabela_fipe_spring/model/Modelos.java
@@ -2,6 +2,8 @@ package br.com.alura.tabela_fipe_spring.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Modelos(List<Dados> modelos) {
+public record Modelos(List<Data> modelos) {
 }
