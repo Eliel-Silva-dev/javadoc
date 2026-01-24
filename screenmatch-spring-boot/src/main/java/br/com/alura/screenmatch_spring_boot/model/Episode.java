a3dbe0c59386a6e41ccf92a38016f1ffe6bbdfa6
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-24 15:36:42 -0300

    feat: new model episode

---
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/model/Episode.java
@@ -0,0 +1,79 @@
+package br.com.alura.screenmatch_spring_boot.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episode {
+    private Integer numberSeason;
+    private String title;
+    private Integer numberEpisode;
+    private Double evaluation;
+    private LocalDate realiseDate;
+
+    public Episode(Integer numberSeason, DataEpisode dataEpisode) {
+        this.numberSeason = numberSeason;
+        this.title = dataEpisode.title();
+        this.numberEpisode = dataEpisode.number();
+
+        try {
+            this.evaluation = Double.valueOf(dataEpisode.rating());
+        } catch (NumberFormatException ex) {
+            this.evaluation = 0.0;
+        }
+
+        try {
+            this.realiseDate = LocalDate.parse(dataEpisode.ReleaseDate());
+        } catch (DateTimeParseException ex) {
+            this.realiseDate = null;
+        }
+    }
+
+    public Integer getNumberSeason() {
+        return numberSeason;
+    }
+
+    public void setNumberSeason(Integer numberSeason) {
+        this.numberSeason = numberSeason;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getNumberEpisode() {
+        return numberEpisode;
+    }
+
+    public void setNumberEpisode(Integer numberEpisode) {
+        this.numberEpisode = numberEpisode;
+    }
+
+    public Double getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(Double evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    public LocalDate getRealiseDate() {
+        return realiseDate;
+    }
+
+    public void setRealiseDate(LocalDate realiseDate) {
+        this.realiseDate = realiseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "temporada=" + numberSeason +
+                ", titulo='" + title + '\'' +
+                ", numeroEpisodio=" + numberEpisode +
+                ", avaliacao=" + evaluation +
+                ", dataLancamento=" + realiseDate;
+    }
+}
