7113e91dae45f92e3f5f059f98b495e87ee446c3
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-25 20:33:52 -0300

    feat: criando model enum

---
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/model/Categoria.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatch_spring_boot.model;
+
+public enum Categoria {
+    ACAO("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIME("Crime");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+}
