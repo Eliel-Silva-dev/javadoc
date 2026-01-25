b1e17fb08c563cba2a097cdddc3953d149c3d10a
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-24 21:20:24 -0300

    feat: request marcas + modelos

--- tabela-fipe-spring/src/main/java/br/com/alura/tabela_fipe_spring/principal/Principal.java
+++ tabela-fipe-spring/src/main/java/br/com/alura/tabela_fipe_spring/principal/Principal.java
@@ -1,18 +1,21 @@
 package br.com.alura.tabela_fipe_spring.principal;
 
+import br.com.alura.tabela_fipe_spring.model.Data;
+import br.com.alura.tabela_fipe_spring.model.Modelos;
 import br.com.alura.tabela_fipe_spring.service.ConsumoApi;
 import br.com.alura.tabela_fipe_spring.service.ConverterDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
-    private Scanner scan = new Scanner(System.in);
-    private ConsumoApi consumo = new ConsumoApi();
-    private ConverterDados conversor = new ConverterDados();
+    private final Scanner scan = new Scanner(System.in);
+    private final ConsumoApi consumo = new ConsumoApi();
+    private final ConverterDados conversor = new ConverterDados();
 
     private String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
-    public void showMenu () {
+    public void showMenu() {
         var menu = """
                 *** OPÇÔES ***
                 Carro
@@ -33,6 +36,25 @@ public class Principal {
         } else {
             address = URL_BASE + "caminhoes/marcas";
         }
+
+        var json = consumo.obterDados(address);
+        System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Data.class);
+        marcas.stream().sorted(Comparator.comparing(Data::code))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para consulta");
+        var codigoMarca = scan.nextLine();
+
+        address = address + "/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(address);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("Modelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Data::code))
+                .forEach(System.out::println);
 
         
     }
