a4dc33eab2b90ad2863ff95c8b79a5e3ee0be2ef
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-26 21:43:19 -0300

    feat: obtendo tradução de sinopse via chat gpt

--- screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/service/ConsultaChatGPT.java
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/service/ConsultaChatGPT.java
@@ -4,18 +4,29 @@ import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
+    private final static String apiKey = System.getenv("OPENAI_API_KEY");
 
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("chave openai");
+        try {
+            OpenAiService service = new OpenAiService(apiKey);
 
-        CompletionRequest requisicao = CompletionRequest.builder()
-                .model("gpt-3.5-turbo-instruct")
-                .prompt("Traduza para o português o texto:" + texto)
-                .maxTokens(1000)
-                .temperature(0.7)
-                .build();
+            CompletionRequest requisicao = CompletionRequest.builder()
+                    .model("gpt-3.5-turbo-instruct")
+                    .prompt("Traduza para o português o texto:" + texto)
+                    .maxTokens(1000)
+                    .temperature(0.7)
+                    .build();
 
-        var resposta = service.createCompletion(requisicao);
-        return resposta.getChoices().getFirst().getText();
+            var resposta = service.createCompletion(requisicao);
+            System.out.println(resposta);
+            System.out.println(resposta.getChoices());
+            System.out.println(resposta.getChoices().get(0));
+            System.out.println(resposta.getChoices().get(0).getText());
+            //return resposta.getChoices().get(0).getText();
+            return "Alguma coisa...";
+        } catch (Exception e) {
+            System.out.println("Erro da requisição da api: " + e.getMessage());
+            return "Erro ao tentar obter a tradução";
+        }
     }
 }
--- screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/service/ConverteDados.java
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/service/ConverteDados.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ConverteDados implements IConvertData {
     private ObjectMapper mapper = new ObjectMapper();
 
+
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
         try {
