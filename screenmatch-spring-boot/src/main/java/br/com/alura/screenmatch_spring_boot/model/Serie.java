463a3174593a8ea58ded0ccdd50d66412f482ef1
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-26 23:16:37 -0300

    feat: chamando nova classe de traducao

--- screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/model/Serie.java
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/model/Serie.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch_spring_boot.model;
 
 import br.com.alura.screenmatch_spring_boot.service.ConsultaChatGPT;
+import br.com.alura.screenmatch_spring_boot.service.traducao.ConsultaMyMemory;
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 import java.util.OptionalDouble;
@@ -21,8 +22,8 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        //this.sinopse = dadosSerie.sinopse();
-        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+        this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
+        //this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
     public String getTitulo() {
