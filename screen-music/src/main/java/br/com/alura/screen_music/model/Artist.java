b022a74c270823551284f1456bbd6ab6ba4ec3d4
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-01-31 20:20:25 -0300

    feat: model artist

---
+++ screen-music/src/main/java/br/com/alura/screen_music/model/Artist.java
@@ -0,0 +1,73 @@
+package br.com.alura.screen_music.model;
+
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+import br.com.alura.screen_music.model.Music;
+
+@Entity
+@Table(name = "artistas")
+public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private ArtistType type;
+
+    @OneToMany(mappedBy = "artista", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Music> musics = new ArrayList<>();
+
+    public Artist() {
+    }
+
+    public Artist(String name, ArtistType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArtistType getType() {
+        return type;
+    }
+
+    public void setType(ArtistType type) {
+        this.type = type;
+    }
+
+    public List<Music> getMusics() {
+        return musics;
+    }
+
+    public void setMusics(List<Music> musics) {
+        this.musics = musics;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista = " + name +
+                ", tipo = " + type +
+                ", musicas = " + musics;
+    }
+}
