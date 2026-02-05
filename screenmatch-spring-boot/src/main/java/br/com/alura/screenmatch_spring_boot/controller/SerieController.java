9b16ee761a31f1c944a20f65b01fe9296c1cd856
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-02-05 19:02:31 -0300

    feat: mapeamento de rotas series

--- screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/controller/SerieController.java
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/controller/SerieController.java
@@ -1,13 +1,22 @@
 package br.com.alura.screenmatch_spring_boot.controller;
 
+import br.com.alura.screenmatch_spring_boot.model.Serie;
+import br.com.alura.screenmatch_spring_boot.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repositorio;
+
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui vão retornar as séries";
+    public List<Serie> obterSeries() {
+        return repositorio.findAll();
     }
 }
