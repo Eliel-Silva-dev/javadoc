c14da56df5cfb8f574a73a62433ae66f827f114f
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-02-06 19:31:01 -0300

    feat: add cors config

---
+++ screenmatch-spring-boot/src/main/java/br/com/alura/screenmatch_spring_boot/config/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch_spring_boot.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5501")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
