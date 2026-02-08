713b044f0576294ccf55d4f3ee7e12df55dce6d9
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-02-08 20:05:36 -0300

    feat: dto de frase

---
+++ screenMatchFrases/src/main/java/br/com/alura/screenMatchFrases/dto/FraseDTO.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenMatchFrases.dto;
+
+public record FraseDTO(
+        String titulo,
+
+        String frase,
+
+        String personagem,
+
+        String poster
+) {
+}
