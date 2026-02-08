d9cfea6edcfb325e9cd4f8ee05216a7c14c3dada
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-02-08 20:06:10 -0300

    feat: add frase service

---
+++ screenMatchFrases/src/main/java/br/com/alura/screenMatchFrases/service/FraseService.java
@@ -0,0 +1,19 @@
+package br.com.alura.screenMatchFrases.service;
+
+import br.com.alura.screenMatchFrases.dto.FraseDTO;
+import br.com.alura.screenMatchFrases.model.Frase;
+import br.com.alura.screenMatchFrases.repository.FraseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FraseService {
+
+    @Autowired
+    private FraseRepository repositorio;
+
+    public FraseDTO obterFraseAleatoria() {
+        Frase frase = repositorio.buscaFraseAleatoria();
+        return new FraseDTO(frase.getTitulo(), frase.getFrase(), frase.getPersonagem(), frase.getPoster());
+    }
+}
