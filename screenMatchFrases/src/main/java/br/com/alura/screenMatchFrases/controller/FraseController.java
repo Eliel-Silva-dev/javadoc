6f51bf8e39e8a3e72fdd339c378984ff941484bd
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-02-08 20:06:55 -0300

    feat: add imports , gets e sets
    frasecontroller + frase model

--- screenMatchFrases/src/main/java/br/com/alura/screenMatchFrases/controller/FraseController.java
+++ screenMatchFrases/src/main/java/br/com/alura/screenMatchFrases/controller/FraseController.java
@@ -1,5 +1,7 @@
 package br.com.alura.screenMatchFrases.controller;
 
+import br.com.alura.screenMatchFrases.dto.FraseDTO;
+import br.com.alura.screenMatchFrases.service.FraseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
