c56c4df0347ae8e8238f8b9aaea82dc496c2553f
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-04-09 19:27:49 -0300

    feat: new project
    arquivos base do projeto de reserva de sala

---
+++ reservaSala/src/main/java/com/alura/reservaSala/ReservaSala.java
@@ -0,0 +1,15 @@
+package com.alura.reservaSala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSala {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(ReservaSala.class, args);
+
+		System.out.println("Testando projeto");
+	}
+
+}
