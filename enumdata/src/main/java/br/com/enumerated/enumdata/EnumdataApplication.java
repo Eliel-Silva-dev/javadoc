c99521f38565c70e4d76a897ee1a6db6fc7d5a7c
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-05-07 20:26:43 -0300

    new project enum data

---
+++ enumdata/src/main/java/br/com/enumerated/enumdata/EnumdataApplication.java
@@ -0,0 +1,13 @@
+package br.com.enumerated.enumdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumdataApplication.class, args);
+	}
+
+}
---
+++ enumdata/src/test/java/br/com/enumerated/enumdata/EnumdataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.enumerated.enumdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnumdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
