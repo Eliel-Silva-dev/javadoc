4827c4eefbee6a0e29b89b963c0588cd5ffce2e9
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-05-16 18:37:47 -0300

    feat: valida cpf
    valida cpf, remove caracteres especiais e imprime cnpj

--- enumdata/src/main/java/br/com/enumerated/aula01/GeraCpfCnpj.java
+++ enumdata/src/main/java/br/com/enumerated/aula01/GeraCpfCnpj.java
@@ -53,7 +53,7 @@ public class GeraCpfCnpj {
         return retorno;
     }
 
-    public static String cnpf() {
+    public static String cnpj() {
         int n = 9;
         int n1 = randomiza(n);
         int n2 = randomiza(n);
@@ -87,5 +87,85 @@ public class GeraCpfCnpj {
             retorno = "" + n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + n11 + n12 + d1 + d2;
         }
         return retorno;
+    }
+
+    public boolean isCPF(String CPF) {
+
+        CPF = removeCaracteresEspeciais(CPF);
+
+        // consifera-se erro CPF's compostos por uma sequencia de numeros iguais
+        if (CPF.equals("00000000000") || CPF.equals("11111111111") || CPF.equals("22222222222") || CPF.equals("33333333333") || CPF.equals("44444444444") || CPF.equals("55555555555") || CPF.equals("66666666666") || CPF.equals("77777777777") || CPF.equals("88888888888") || CPF.equals("99999999999") || (CPF.length() != 11))
+            return (false);
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        // esse bloco try protege o código de eventuais erros de conversão de tipo - int
+        try {
+            //calcando o primeiro digito verificador
+            sm = 0;
+            peso = 10;
+
+            for (i = 0; i < 9; i++) {
+                // converte o i-esimo caractere do CPF em um numero:
+                // por exemplo, transforma o caractere '0' no inteiro 0
+                // (48 eh a posicao de '0' na tabela ASCII)
+                num = (int) (CPF.charAt(i) - 48);
+                sm += (num * peso);
+                peso --;
+            }
+
+            r = 11 - (sm % 11);
+            if((r == 10) || (r == 11)) {
+                dig10 = '0';
+            } else {
+                dig10 = (char) (r + 48); //converter para o respectivo caractere numérico;
+            }
+
+            // Calculo do 2o. Digito Verificador
+            sm = 0;
+            peso = 11;
+            for (i = 0; i < 10; i++) {
+                num = (int) (CPF.charAt(i) - 48);
+                sm += (num * peso);
+                peso --;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11)) {
+                dig11 = '0';
+            } else {
+                dig11 = (char) (r + 48);
+            }
+
+            //verifica se os numeros calculados coincidem com os numeros passados por parametro
+            if((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10))) {
+                return (true);
+            } else {
+                return (true);
+            }
+        } catch (InputMismatchException e) {
+            return (false);
+        }
+    }
+
+    private String removeCaracteresEspeciais( String doc) {
+
+        if (doc.contains(".")){
+            doc = doc.replace(".", "");
+        }
+        if (doc.contains("-")){
+            doc = doc.replace("-", "");
+        }
+        if (doc.contains("/")){
+            doc = doc.replace("/", "");
+        }
+
+        return doc;
+    }
+
+    public static String imprimeCNPJ(String CNPJ) {
+        // máscara do CNPJ: 99.999.999.9999-99
+        return (CNPJ.substring(0, 2) + "." + CNPJ.substring(2, 5) + "." + CNPJ.substring(5, 8) + "." + CNPJ.substring(8, 12) + "-" + CNPJ.substring(12, 14));
     }
 }
