5ffb3104b956595baa67fb0fc60d67f53c23d6eb
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-06-09 19:58:44 -0300

    docs: entities user and department

---
+++ userdepartment/src/main/java/com/hydrahtec/userdepartment/entities/Department.java
@@ -0,0 +1,34 @@
+package com.hydrahtec.userdepartment.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_department")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    private User[] users;
+
+    public Department() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
