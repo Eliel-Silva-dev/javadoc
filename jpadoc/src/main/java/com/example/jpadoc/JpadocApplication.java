cd9a6c47beb2fdec800056519f961986001ceaa2
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-06-19 23:51:19 -0300

    feat: new project data jpa doc

---
+++ jpadoc/src/main/java/com/example/jpadoc/JpadocApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpadoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpadocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpadocApplication.class, args);
+	}
+
+}
---
+++ jpadoc/src/test/java/com/example/jpadoc/JpadocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpadoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpadocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
