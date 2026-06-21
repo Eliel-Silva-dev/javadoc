9dcb882bdd33bb5bb71707f7aa6ef2a6fbbe02fd
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-06-21 04:45:12 -0300

    refact: rename package

--- jpadoc/src/main/java/com/hydrahtec/jpadoc/JpadocApplication.java
+++ jpadoc/src/main/java/com/hydrahtec/jpadoc/JpadocApplication.java
@@ -1,4 +1,4 @@
-package com.example.jpadoc;
+package com.hydrahtec.jpadoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
