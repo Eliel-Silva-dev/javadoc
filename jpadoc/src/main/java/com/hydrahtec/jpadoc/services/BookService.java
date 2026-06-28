9537be647d35a4be9020aff9a3df447a28a18eba
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-06-28 11:25:31 -0300

    feat: book service

---
+++ jpadoc/src/main/java/com/hydrahtec/jpadoc/services/BookService.java
@@ -0,0 +1,43 @@
+package com.hydrahtec.jpadoc.services;
+
+import com.hydrahtec.jpadoc.dtos.BookRecordDto;
+import com.hydrahtec.jpadoc.models.BookModel;
+import com.hydrahtec.jpadoc.models.ReviewModel;
+import com.hydrahtec.jpadoc.repositories.AuthorRepository;
+import com.hydrahtec.jpadoc.repositories.BookRepository;
+import com.hydrahtec.jpadoc.repositories.PublisherRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+    private final AuthorRepository authorRepository;
+
+    public BookService(BookRepository bookRepository, PublisherRepository publisherRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    @Transactional
+    public BookModel saveBook(BookRecordDto bookRecordDto) {
+        BookModel book = new BookModel();
+
+        book.setTitle(bookRecordDto.title());
+        book.setPublisher(publisherRepository.findById(bookRecordDto.publisherId()).get());
+        book.setAuthors(authorRepository.findAllById(bookRecordDto.authorIds()).stream().collect(Collectors.toSet()));
+
+        ReviewModel reviewModel = new ReviewModel();
+        reviewModel.setComment(bookRecordDto.reviewComment());
+        reviewModel.setBook(book);
+
+        book.setReview(reviewModel);
+
+        return bookRepository.save(book);
+    }
+}
