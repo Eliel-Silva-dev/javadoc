b6a571e67a134740d941766ffe88da27797eaad5
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-01 23:27:28 -0300

    feat: new project
    new project, exception documentation

---
+++ exceptiondoc/src/main/java/com/hydrahtec/exceptiondoc/ExceptiondocApplication.java
@@ -0,0 +1,13 @@
+package com.hydrahtec.exceptiondoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptiondocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptiondocApplication.class, args);
+	}
+
+}
---
+++ exceptiondoc/src/test/java/com/hydrahtec/exceptiondoc/ExceptiondocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydrahtec.exceptiondoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptiondocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
