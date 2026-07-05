fe074fc9cf15dc28321778586d5768c14ce54531
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-04 23:56:41 -0300

    feat: new project spring dsaula05

---
+++ dsaula05/src/main/java/com/hydrahtec/dsaula05/Dsaula05Application.java
@@ -0,0 +1,13 @@
+package com.hydrahtec.dsaula05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsaula05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsaula05Application.class, args);
+	}
+
+}
