1b80a70d1964cf79f439869ffa2fce74bcd90dc9
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-06 23:54:03 -0300

    feat: get mapping by id category

--- dsaula05/src/main/java/com/hydrahtec/dsaula05/controllers/CategoryController.java
+++ dsaula05/src/main/java/com/hydrahtec/dsaula05/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     @GetMapping
     public ResponseEntity<> findAll() {
+
+        return ResponseEntity.status(HttpStatus.OK).body();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<> findById(@RequestParam Long id) {
+        
 
         return ResponseEntity.status(HttpStatus.OK).body();
     }
