0f0819b3a8b9409bd42846e0898ee1a8952387fd
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-07 00:51:42 -0300

    feat: moqueando retorno get category

--- dsaula05/src/main/java/com/hydrahtec/dsaula05/controllers/CategoryController.java
+++ dsaula05/src/main/java/com/hydrahtec/dsaula05/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package com.hydrahtec.dsaula05.controllers;
 
+import entities.CategoryEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,19 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/caegories")
+@RequestMapping(value = "/categories")
 public class CategoryController {
     @GetMapping
-    public ResponseEntity<> findAll() {
+    public ResponseEntity<List<CategoryEntity>> findAll() {
+        List<CategoryEntity> categories = new ArrayList<>();
+        categories.add(new CategoryEntity(1L, "Eletronics"));
+        categories.add(new CategoryEntity(2L, "Books"));
 
-        return ResponseEntity.status(HttpStatus.OK).body();
+        return ResponseEntity.status(HttpStatus.OK).body(categories);
     }
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<> findById(@RequestParam Long id) {
-        
-
-        return ResponseEntity.status(HttpStatus.OK).body();
-    }
+//    @GetMapping(value = "/{id}")
+//    public ResponseEntity<> findById(@RequestParam Long id) {
+//
+//
+//        return ResponseEntity.status(HttpStatus.OK).body();
+//    }
 }
