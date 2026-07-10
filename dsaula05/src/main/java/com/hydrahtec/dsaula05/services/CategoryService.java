cd5fb6ba51c7c226c3402c947a232d2976291bb9
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-10 00:52:12 -0300

    feat: category service find all

---
+++ dsaula05/src/main/java/com/hydrahtec/dsaula05/services/CategoryService.java
@@ -0,0 +1,34 @@
+package com.hydrahtec.dsaula05.services;
+
+import com.hydrahtec.dsaula05.entities.CategoryEntity;
+import com.hydrahtec.dsaula05.exceptions.CategoryNotFoundException;
+import com.hydrahtec.dsaula05.repositories.CategoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    private static final Logger log = LoggerFactory.getLogger(CategoryService.class);
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<CategoryEntity> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+//    public CategoryEntity findCategoryById(Long id) {
+//        Optional<CategoryEntity> category = categoryRepository.findById(id);
+//
+//        if(category.isPresent()) {
+//            return category.get();
+//        } else {
+//            throw new CategoryNotFoundException("Category not found");
+//        }
+//    }
+}
