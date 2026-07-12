1ffff5598547e0eea596a523ed250e237207fb88
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-12 13:26:52 -0300

    refact: reformulando metodo handler exception

--- dsaula05/src/main/java/com/hydrahtec/dsaula05/exceptions/GlobalExceptionHandler.java
+++ dsaula05/src/main/java/com/hydrahtec/dsaula05/exceptions/GlobalExceptionHandler.java
@@ -1,26 +1,28 @@
 package com.hydrahtec.dsaula05.exceptions;
 
-import org.springframework.http.HttpRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({CategoryNotFoundException.class, ProductNotFoundException.class})
-    public ResponseEntity<RestErrorMessage> notFoundHandler(Exception ex, HttpRequest request) {
-
-        RestErrorMessage error = new RestErrorMessage(
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public RestErrorMessage resourceNotFoundHandler(Exception ex, HttpServletRequest request) {
+        return new RestErrorMessage(
                 LocalDateTime.now(),
-                HttpStatus.NOT_FOUND,
+                HttpStatus.NOT_FOUND.value(),
                 ex.getMessage(),
-                request.getURI().getPath()
+                "Resource Not Found",
+                request.getRequestURI()
         );
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
