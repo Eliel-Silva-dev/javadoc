97044e1a0b286ee5659ace551869a29976281a25
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-13 01:22:36 -0300

    feat: new method to catch exception validate

--- dsaula05/src/main/java/com/hydrahtec/dsaula05/exceptions/GlobalExceptionHandler.java
+++ dsaula05/src/main/java/com/hydrahtec/dsaula05/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.hydrahtec.dsaula05.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -34,5 +40,28 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 request.getRequestURI()
         );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+
+        List<String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage()).toList();
+
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+        RestErrorMessage errorMessage = new RestErrorMessage(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Foram encontrados erros nos campos preenchidos",
+                "Validation error",
+                path,
+                errors
+        );
+
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 }
