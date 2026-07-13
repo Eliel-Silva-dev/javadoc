5f3382feaf2482cdef6d65e53abc270bfdc5cb79
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-13 00:28:15 -0300

    refact: create secondary constructor error message

--- dsaula05/src/main/java/com/hydrahtec/dsaula05/exceptions/RestErrorMessage.java
+++ dsaula05/src/main/java/com/hydrahtec/dsaula05/exceptions/RestErrorMessage.java
@@ -1,11 +1,22 @@
 package com.hydrahtec.dsaula05.exceptions;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 
 public record RestErrorMessage(
         LocalDateTime timestamp,
         int status,
         String message,
         String error,
-        String path) {
+        String path,
+        List<String> errors) {
+    public RestErrorMessage(LocalDateTime timestamp,
+                            int status,
+                            String message,
+                            String error,
+                            String path) {
+        this(timestamp, status, message, error, path, Collections.emptyList());
+
+    }
 }
