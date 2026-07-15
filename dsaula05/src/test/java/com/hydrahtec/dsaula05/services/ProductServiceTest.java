bcf26f783ee0af28492012aff02300c5b1829c6b
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-14 23:45:16 -0300

    feat: class test

---
+++ dsaula05/src/test/java/com/hydrahtec/dsaula05/services/ProductServiceTest.java
@@ -0,0 +1,13 @@
+package com.hydrahtec.dsaula05.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductServiceTest {
+
+    @Test
+    void
+}
