ab15aaeee0e30beb7a61cbec1177e24c241452e9
Eliel-Silva-dev <eliellnr@hotmail.com>
2026-07-17 01:07:46 -0300

    test: categoryService throw ex if not exist id

--- dsaula05/src/test/java/com/hydrahtec/dsaula05/services/CategoryServiceTest.java
+++ dsaula05/src/test/java/com/hydrahtec/dsaula05/services/CategoryServiceTest.java
@@ -1,6 +1,7 @@
 package com.hydrahtec.dsaula05.services;
 
 import com.hydrahtec.dsaula05.entities.CategoryEntity;
+import com.hydrahtec.dsaula05.exceptions.CategoryNotFoundException;
 import com.hydrahtec.dsaula05.models.CategoryDto;
 import com.hydrahtec.dsaula05.repositories.CategoryRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,7 +28,7 @@ class CategoryServiceTest {
 
     @Test
     @DisplayName("Deve retornar lista de categorias se existir dados")
-    public void sould_return_categoryList_when_ExistData() {
+    public void should_return_categoryList_when_ExistData() {
         //ARRANGE
         CategoryEntity category = new CategoryEntity(1L, "Eletronics");
         when(categoryRepository.findAll()).thenReturn(List.of(category));
@@ -41,7 +43,7 @@ class CategoryServiceTest {
 
     @Test
     @DisplayName("Deve retornar categorias se id existir")
-    public void sould_return_category_when_ExistId() {
+    public void should_return_category_when_ExistId() {
         //ARRANGE
         Long id = 1L;
         CategoryEntity category = new CategoryEntity(id, "Books");
@@ -53,5 +55,18 @@ class CategoryServiceTest {
         //ASSERTIONS
         assertThat(result).isNotNull();
         assertThat(result.name()).isEqualTo("Books");
+    }
+
+    @Test
+    @DisplayName("Deve lançar exception se id não existir")
+    public void should_throwException_when_NotExistId() {
+        //ARRANGE
+        Long id = 99L;
+        when(categoryRepository.findById(id)).thenReturn(Optional.empty());
+
+        //ACT and ASSERTIONS
+        assertThatThrownBy(() -> categoryService.findCategoryById(id))
+                .isInstanceOf(CategoryNotFoundException.class)
+                .hasMessage("Categoria não encontrada, ID: 99");
     }
 }
